950a3c22bf08b828caa66f60e06b1d6abdc9e021
Yogendra Rana <yogendrarana4321@gmail.com>
2023-11-11 21:33:32 +0545

    Password update functionality added

--- src/app/api/auth/password/route.ts
+++ src/app/api/auth/password/route.ts
@@ -1,7 +1,46 @@
-export async function UPDATE () {
+import bcrypt from "bcrypt";
+import { db } from "@/src/db/db";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import { userSchema } from "@/drizzle/schema/user.schema";
+
+export async function PUT(req: NextRequest, res: NextResponse) {
+    const url = new URL(req.url);
+    const email = url.searchParams.get("email")
+
+    const body = await req.json();
+    const { oldPassword, newPassword } = body;
+
     try {
+        if (email == null) {
+            return NextResponse.json({ success: false, message: "Please provide email." }, { status: 400 });
+        }
 
-    }catch (err: any) {
+        // check if the user exists
+        const user = await db.query.userSchema.findFirst({where: eq(userSchema.email, email)})
+        if (!user) {
+            return NextResponse.json({ success: false, message: `${email} is not registered.` }, { status: 400 });
+        }
 
+
+        // check if the passwords are empty or not
+        if (!oldPassword || !newPassword) {
+            return NextResponse.json({ success: false, message: "Please provide old and new password." }, { status: 400 });
+        }
+
+        // check if the old password is correct
+        const passwordMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!passwordMatch) return NextResponse.json({ success: false, message: "Old password is incorrect." }, { status: 400 });
+
+        // hash the new password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        // update the password
+        await db.update(userSchema).set({ password: hashedPassword }).where(eq(userSchema.email, email));
+
+        // return success message
+        return NextResponse.json({ success: true, message: 'Password updated successfully.' }, { status: 200 });
+    } catch (err: any) {
+        return NextResponse.json({ success: false, message: err.message }, { status: err.status || 500 });
     }
 }
--- src/components/user/UpdateUserDetail.tsx
+++ src/components/user/UpdateUserDetail.tsx
@@ -2,6 +2,7 @@
 
 
 import axios from 'axios';
+import { set } from 'date-fns';
 import { useSession } from 'next-auth/react';
 import React from 'react';
 import toast from 'react-hot-toast';
@@ -11,7 +12,8 @@ const UpdateUserDetail = () => {
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
     const [oldPassword, setOldPassword] = React.useState<string>("");
     const [newPassword, setNewPassword] = React.useState<string>("");
-    const [updateObject, setUpdateObject] = React.useState({newName: "", newEmail: ""});
+    const [showPassword, setShowPassword] = React.useState<boolean>(false);
+    const [updateObject, setUpdateObject] = React.useState({ newName: "", newEmail: "" });
 
 
     // session
@@ -21,7 +23,7 @@ const UpdateUserDetail = () => {
 
     // handle data change
     const handleChangedData = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUpdateObject({...updateObject, [e.target.id]: e.target.value});
+        setUpdateObject({ ...updateObject, [e.target.id]: e.target.value });
     }
 
 
@@ -37,10 +39,10 @@ const UpdateUserDetail = () => {
 
             // reauthenticate user
             if (updateObject.newEmail) {
-                const res = await session.update({email: updateObject.newEmail});
-                console.log("res", res);
-            } 
+                await session.update({ email: updateObject.newEmail });
+            }
 
+            setUpdateObject({ newName: "", newEmail: "" });
             setIsLoading(false);
         } catch (err: any) {
             setIsLoading(false);
@@ -49,17 +51,22 @@ const UpdateUserDetail = () => {
     }
 
 
-    // change password
+    // handle change password
     const handleChangePassword = async () => {
+        setIsLoading(true);
         const toastId = toast.loading("Updating password...");
-        const updatePassword = {newPassword, oldPassword, email}
+        const updatePasswordObject = { newPassword, oldPassword }
 
         try {
-            const res = await axios.post("/api/auth/password/change", updatePassword)
-            if (res.status >= 3000) throw Error(res.data.message);
+            const res = await axios.put(`/api/auth/password?email=${email}`, {...updatePasswordObject})
+            if (res.status >= 300) throw Error(res.data.message);
             toast.success(res.data.message, { id: toastId });
-        }catch (err: any){
-            console.log(err.message);
+
+            setOldPassword("");
+            setNewPassword("");
+            setIsLoading(false);
+        } catch (err: any) {
+            setIsLoading(false);
             toast.error(err.response.data.message, { id: toastId });
         }
     }
@@ -76,8 +83,9 @@ const UpdateUserDetail = () => {
                             type="text"
                             id="newName"
                             value={updateObject.newName}
+                            autoComplete='off'
                             onChange={(e) => handleChangedData(e)}
-                            className="w-full p-[0.75rem] border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
+                            className="w-full p-[0.75rem] outline-none border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
                         />
                     </div>
                     <div>
@@ -86,8 +94,9 @@ const UpdateUserDetail = () => {
                             type="email"
                             id="newEmail"
                             value={updateObject.newEmail}
+                            autoComplete='off'
                             onChange={(e) => handleChangedData(e)}
-                            className="w-full p-[0.75rem] border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
+                            className="w-full p-[0.75rem] outline-none border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
                         />
                     </div>
                     <button
@@ -96,39 +105,50 @@ const UpdateUserDetail = () => {
                         disabled={isLoading}
                         className={`flex-1 p-[0.75rem] rounded-[0.5rem] bg-gray-200 text-[1.25rem] duration-200 ${isLoading && "opacity-50 cursor-not-allowed"}`}
                     >
-                        Save 
+                        Save
                     </button>
                 </div>
 
 
                 <div className="w-full flex flex-col gap-[1rem]">
                     <div>
-                        <label htmlFor="old_password" className="text-[1.25rem] block text-gray-700 font-bold">Old Password</label>
+                        <label htmlFor="old_password" className="flex text-[1.25rem] text-gray-700 font-bold">
+                            <span className='mr-auto'>Old Password</span>
+                            <button className='text-gray-400' onClick={() => setShowPassword(!showPassword)}>
+                                {showPassword ? (<i className='fa-solid fa-eye-slash'></i>) : (<i className='fa-solid fa-eye'></i>)}
+                            </button>
+                        </label>
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             id="old_password"
                             autoComplete="new-password"
                             value={oldPassword}
                             onChange={(e) => setOldPassword(e.target.value)}
-                            className="w-full p-[0.75rem] border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
+                            className="w-full p-[0.75rem] outline-none border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
                         />
                     </div>
                     <div>
-                        <label htmlFor="new_password" className="text-[1.25rem] block text-gray-700 font-bold">New Password</label>
+                        <label htmlFor="new_password" className="flex text-[1.25rem] text-gray-700 font-bold">
+                            <span className='mr-auto'>New Password</span>
+                            <button className='text-gray-400' onClick={() => setShowPassword(!showPassword)}>
+                                
+                                {showPassword ? (<i className='fa-solid fa-eye-slash'></i>) : (<i className='fa-solid fa-eye'></i>)}
+                            </button>
+                        </label>
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             id="new_password"
                             autoComplete="new-password"
                             value={newPassword}
                             onChange={(e) => setNewPassword(e.target.value)}
-                            className="w-full p-[0.75rem] border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
+                            className="w-full p-[0.75rem] outline-none border rounded-[0.5rem] text-gray-700 text-[1.25rem]"
                         />
                     </div>
 
                     <button
                         type="button"
                         disabled={isLoading}
-                    onClick={handleChangePassword}
+                        onClick={handleChangePassword}
                         className={`flex-1 p-[0.75rem] rounded-[0.5rem] bg-gray-200 text-[1.25rem] duration-200 ${isLoading && "opacity-50 cursor-not-allowed"}`}
                     >
                         Change Password
