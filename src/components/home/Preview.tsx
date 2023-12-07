bd616800b28c49de25c394de48a0be14aee17842
Yogendra Rana <yogendrarana4321@gmail.com>
2023-12-07 13:15:34 +0545

    Improved preview

--- src/components/home/Preview.tsx
+++ src/components/home/Preview.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 // import image
 import Preview1 from '@/src/assets/images/preview-1.png'
@@ -8,17 +8,21 @@ import Preview2 from '@/src/assets/images/preview-2.png'
 const Preview = () => {
     return (
         <div className="">
-            <h2 className="mb-[2rem] text-[5rem] font-bold text-center">Preview</h2>
+            <h2
+                className="mb-[2rem] text-[5rem] font-bold text-center"
+            >
+                Preview
+            </h2>
 
             <div className="flex gap-[1rem] sm:flex-col">
-                <motion.div 
+                <motion.div
                     className="relative border rounded-[1rem] overflow-hidden"
                     initial={{ y: "20%", opacity: 0 }}
-                    whileInView={{ y: "0%", opacity: 1 }} 
+                    whileInView={{ y: "0%", opacity: 1 }}
                     transition={{ duration: 0.3 }}
                 >
                     <Image src={Preview1} alt="chat image" />
-                    <div 
+                    <div
                         className="
                             absolute inset-0 
                             opacity-[0.25] 
@@ -29,14 +33,14 @@ const Preview = () => {
                     </div>
                 </motion.div>
 
-                <motion.div 
+                <motion.div
                     className="relative border rounded-[1rem] overflow-hidden"
                     initial={{ y: "20%", opacity: 0 }}
-                    whileInView={{ y: "0%", opacity: 1 }} 
+                    whileInView={{ y: "0%", opacity: 1 }}
                     transition={{ duration: 0.5 }}
                 >
                     <Image src={Preview2} alt="chat image" />
-                    <div 
+                    <div
                         className="
                             absolute inset-0 
                             opacity-[0.25] 
